6abb26cb2e16c080de2083b11355c20da3b0dfc8
Filip Czabator <fczabator@gmail.com>
2019-05-20 18:47:16 +0200

    Fixed sound

--- src/App.js
+++ src/App.js
@@ -2,33 +2,38 @@ import React from 'react';
 import {Count} from './Count';
 import './App.css';
 import Rythm from 'rythm.js'
-const rythm = new Rythm();
-rythm.setMusic('sandstorm.mp3');
-rythm.addRythm('code', 'pulse', 0, 10)
-rythm.addRythm('message', 'neon', 0, 10, {
-  from: [0,0,255],
-  to:[255,0,255]
-})
-rythm.addRythm('address', 'jump', 0, 10)
 
-rythm.addRythm('message', 'radius', 0, 10, {
-  min: 0,
-  max: 30
-})
-
-rythm.start();
 
 const COUNT_START = 9;
 
 function App() {
   const [count, setCount] = React.useState(COUNT_START);
   const [show, setShow] = React.useState(false);
+  const [start, setStart] = React.useState(false);
   // let interval = React.useRef(null);
 
+  const handleStart = () => {
+    setStart(true);
+    const rythm = new Rythm();
+    rythm.setMusic('sandstorm.mp3');
+    rythm.addRythm('code', 'pulse', 0, 10)
+    rythm.addRythm('message', 'neon', 0, 10, {
+      from: [0,0,255],
+      to:[255,0,255]
+    })
+    rythm.addRythm('address', 'jump', 0, 10)
+
+    rythm.addRythm('message', 'radius', 0, 10, {
+      min: 0,
+      max: 30
+    })
+    rythm.start();
+  }
+  
   React.useEffect(() => {
     setInterval(() => {
         setCount(prevCount => prevCount - 0.5)
-    }, 1500);
+    }, 1700);
   }, [])
 
   React.useEffect(() => {
@@ -42,13 +47,19 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        
-        {[...Array(COUNT_START).keys()].map(number => (
-          <div key={number} className="count-container">
-            <Count number={number + 1} pose={count === number ? 'visible' : 'hidden'} />
-          </div>
-        ))}
-
+        {!start && (
+          <div className="button" onClick={handleStart}>Start</div>
+        )}
+      
+        {start && (
+          <div>
+            {[...Array(COUNT_START).keys()].map(number => (
+              <div key={number} className="count-container">
+                <Count number={number + 1} pose={count === number ? 'visible' : 'hidden'} />
+              </div>
+            ))}
+          </div>  
+        )}
 
         {show && (
           <div className="message">
